27b946ca226a348ad9656b68d4bf421f3df593f0
Gamouche <cyril.f@hotmail.fr>
2018-04-22 16:09:00 +0200

    just a little function for handling sigwinch, the function does nothing for now .... just preparing

--- srcs/sig_management.c
+++ srcs/sig_management.c
@@ -18,6 +18,14 @@ static void	handle_sigint(int sig)
 	exit(sig);
 }
 
+static void	handle_sigwinch(int sig)
+{
+	(void)sig;
+	/* resize window so re-draw some things.
+	 First, calculate if it's possible to re-print the args, 
+	 if not just print an error msg as "window too small" */
+}
+
 /*
 **	Initialize the functions for handling signals.
 **	If an error occurs, returns -1 or 0 otherwise.
@@ -27,8 +35,7 @@ int			init_sig_handlers(void)
 {
 	if (signal(SIGINT, &(handle_sigint)) == SIG_ERR)
 		return (-1);
-
-
-
+	if (signal(SIGWINCH, &(handle_sigwinch)) == SIG_ERR)
+		return (-1);
 	return (0);
 }
