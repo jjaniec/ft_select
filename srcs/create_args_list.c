41d29296c19489ed1d36e60ce85e768bb2004814
Joffrey Janiec <jjaniec@student.42.fr>
2018-04-24 20:32:56 +0200

    fix last elem & prev ptrs not set without sorting

--- srcs/create_args_list.c
+++ srcs/create_args_list.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/24 19:11:31 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/04/24 19:15:55 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/04/24 20:31:22 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,11 @@ t_ft_select_arg	*create_args_list(t_term_caps *tcaps, char **args)
 		}
 		else
 		{
-			li->next = create_ft_select_arg_struct(args[i], NULL);
+			li->next = create_ft_select_arg_struct(args[i], li);
 			li = li->next;
 		}
 		i++;
 	}
+	tcaps->e_infos.elems_last = li;
 	return (r);
 }
